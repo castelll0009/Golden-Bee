69dfb22fa6dede3adfa901fc68af99a02443d320
castelll0009 <castelll0009@gmail.com>
2021-04-22 10:07:12 -0500

    arreglado aciago

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,5 @@
 var sound = document.getElementById("buzz");
-var btn = document.getElementById("zumbido");
-	
-	
+var btn = document.getElementById("zumbido");	
 
 	function buzzing() {			
 	    sound.play();	   
