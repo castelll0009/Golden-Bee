85dfa9ff62b6eb6e4d837f50b2f63c41cfc235a4
castelll0009 <castelll0009@gmail.com>
2021-04-23 18:07:53 -0500

    mejoras mucuhas

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,8 @@
 var sound = document.getElementById("buzz");
 var btn = document.getElementById("zumbido");	
-var menu = document.getElementsByClassName("container-menu");
-var menuDesplegable = document.getElementsById("menu-desplegable");
-var menuButton= document.getElementsById("menu-button");
 
-	function buzzing() {					
-	    sound.play();			
-	}	
-	
-	function showMenu(){			
-	}
 
+function buzzing() {					
+	sound.play();			
+}		
 	
