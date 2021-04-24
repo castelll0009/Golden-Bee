b556453e665eee425c1bae8b03bf9fa7536b6e84
castelll0009 <castelll0009@gmail.com>
2021-04-24 14:17:49 -0500

    leer mas

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,3 @@
-
-
 var sound = document.getElementById("buzz");
 var btn = document.getElementById("zumbido");	
 var beepc = document.querySelector("#beepc");
@@ -20,3 +18,45 @@ function vistaEscritorio(){
 	}
 	
 }
+function myFunction() {
+	var dots = document.querySelector(".dots");
+	var moreText = document.querySelector(".more");
+	var btnText = document.querySelector(".myBtn");
+
+	/*document.querySelector(".div-figure").style =" background-color: rgb(216, 216, 216); position: absolute; max-height: max-content;overflow: visible;	padding-bottom: 20px; box-shadow: 2px 2px 4px #bbbab8, 10px 10px 10px rgb(141, 141, 141);";
+	document.querySelector(".div-figure").style =" background-color: rgb(216, 216, 216); position: absolute; max-height: max-content;overflow: visible;	padding-bottom: 20px; box-shadow: 2px 2px 4px #bbbab8, 10px 10px 10px rgb(141, 141, 141);";
+	/*position: absolute;
+		max-height: max-content;
+		overflow: visible;
+		padding-bottom: 20px;
+		box-shadow: 2px 2px 4px #bbbab8, 10px 10px 10px rgb(141, 141, 141);		
+		*/
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Read more"; 
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Read less"; 
+	  moreText.style.display = "inline";
+	}
+}
+/*///////////JQUERY///////////////////////////////////////*/
+/*///////////JQUERY///////////////////////////////////////*/
+$(document).ready(function() {	
+	$(".sona").click(function() {
+	  var elem = $("#toggle").text();
+	  if (elem == "Read More") {
+		//Stuff to do when btn is in the read more state
+		$(".puntos").text("");
+		$("#toggle").text("Read Less");
+		$("#text").slideDown();
+	  } else {
+		//Stuff to do when btn is in the read less state
+		$(".puntos").text("...");
+		$("#toggle").text("Read More");
+		$("#text").slideUp();
+	  }
+	});
+  });
