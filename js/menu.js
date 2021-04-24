152087e26dd70c32e9f81b4785eb0c6d430bdcf1
castelll0009 <castelll0009@gmail.com>
2021-04-23 23:24:42 -0500

    zoom melo

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,22 @@
+
+
 var sound = document.getElementById("buzz");
 var btn = document.getElementById("zumbido");	
+var beepc = document.querySelector("#beepc");
 
 
 function buzzing() {					
 	sound.play();			
 }		
+
+var zoom = false;
+function vistaEscritorio(){	
+	if(zoom == false){
+		document.body.style = "zoom: 45%";
+		zoom = true;
+	}else{
+		document.body.style = "zoom: 100%";
+		zoom = false;
+	}
 	
+}
